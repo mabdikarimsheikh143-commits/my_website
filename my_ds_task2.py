8ccb249d82ce8d1385e8e9beb8563cb7d3ea7102
mabdikarimsheikh143-commits <299511525+mabdikarimsheikh143-commits@users.noreply.github.com>
2026-09-25 15:13:58 +0300

    Add files via upload

---
+++ my_ds_task2.py
@@ -0,0 +1,43 @@
+
+my_ds = [45,"Kevin",(720,),["Lesson","Python",{"currency": "KES","student": {"name": "James",
+    "age": 23},"subjects": ["Python", "SQL", "HTML", "CSS"]}],834,(91, "Mary", ["HTML", "CSS", "JavaScript"])
+]
+# 1. Print KES.
+print(type(my_ds))
+print(my_ds[0][1][ "KES"])
+print(my_ds[3][2]['currency'])
+# 2. Print 720 from the tuple.
+print(my_ds[2] [2])
+# 3. Print Python from the nested list.
+print(my_ds[3][1])
+# 4. Print the student's name "James".
+print(my_ds[3][2]["student"]["james"])
+# 5. Print SQL from the subjects list inside the dictionary.
+print(my_ds[3][2]["subjects"][1])
+# 7. Add a new key called "amount" to the dictionary with a value of 1500.
+my_ds[3][2]["amount"]=1500
+# 8. Change the student's name from "James" to "Brian".
+my_ds[3][2]["student"]["name"]="James"
+# 9. Add "Django" to the end of the subjects list.
+my_ds[3][2]["subjects"].append("Django")
+# 10. Change "CSS" in the subjects list to "Bootstrap".
+my_ds[3][2]["subjects"][3]="Bootstrap"
+# 11. Print 834 reversed as 438.
+
+#     Do not use an inbuilt reverse method.
+#     Do not manually assign 438.
+#     Hint: Convert the number to a string and use [::].
+# 12. Print "Mary" from the last tuple.
+# 13. Print "JavaScript" from the list inside the last tuple.
+# 14. Change "JavaScript" to "React".
+# 15. Print the entire updated my_ds.
+
+# BONUS CHALLENGE
+# Print the following individually:
+# Currency: KES
+# Amount: 1500
+# Student: Brian
+# Subject: Django
+# Technology: React
+#
+# You can research or discuss with your classmates to find the solutions.
